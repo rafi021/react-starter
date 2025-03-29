c304028f0592e58cc5b69a3b09f30a66fab0b5b8
rafi021 <mahmud.ibrahim021@gmail.com>
2025-03-29 23:06:13 +0600

    card in auth layout

--- resources/js/pages/Tasks/Create.tsx
+++ resources/js/pages/Tasks/Create.tsx
@@ -1,5 +1,6 @@
 import InputError from '@/components/input-error';
 import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardFooter } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
@@ -54,65 +55,75 @@ const Create = ({ categories }: { categories: TaskCategory[] }) => {
             <Head title="Create Task" />
             <div className="rounde-xl flex h-full flex-1 flex-col gap-4 p-4">
                 <form onSubmit={createTask} className="space-y-6">
-                    <div className="grid gap-2">
-                        <Label htmlFor="name">Task Name</Label>
-                        <Input
-                            id="name"
-                            ref={taskName}
-                            value={data.name}
-                            onChange={(e) => setData('name', e.target.value)}
-                            className="mt-1 block w-full"
-                        />
-                        <InputError message={errors.name} />
-                    </div>
-                    <div className="grid gap-2">
-                        <Label htmlFor="name">Due Date</Label>
+                    <Card>
+                        <CardContent className="grid grid-cols-3 gap-4 space-y-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="name">Task Name *</Label>
 
-                        <Input
-                            id="due_date"
-                            value={data.due_date}
-                            onChange={(e) => setData('due_date', format(new Date(e.target.value), 'yyyy-MM-dd'))}
-                            className="mt-1 block w-full"
-                            type="date"
-                        />
+                                <Input
+                                    id="name"
+                                    ref={taskName}
+                                    value={data.name}
+                                    onChange={(e) => setData('name', e.target.value)}
+                                    className="mt-1 block w-full"
+                                />
 
-                        <InputError message={errors.due_date} />
-                    </div>
-                    <div className="grid gap-2">
-                        <Label htmlFor="media">Media</Label>
+                                <InputError message={errors.name} />
+                            </div>
 
-                        <Input id="media" onChange={(e) => setData('media', e.target.files[0])} className="mt-1 block w-full" type="file" />
+                            <div className="grid gap-2">
+                                <Label htmlFor="name">Due Date</Label>
 
-                        {progress && (
-                            <progress value={progress.percentage} max="100">
-                                {progress.percentage}%
-                            </progress>
-                        )}
+                                <Input
+                                    id="due_date"
+                                    value={data.due_date}
+                                    onChange={(e) => setData('due_date', format(new Date(e.target.value), 'yyyy-MM-dd'))}
+                                    className="mt-1 block w-full"
+                                    type="date"
+                                />
 
-                        <InputError message={errors.media} />
-                    </div>
-                    <div className="grid gap-2">
-                        <Label htmlFor="due_date">Categories</Label>
+                                <InputError message={errors.due_date} />
+                            </div>
 
-                        <ToggleGroup
-                            type="multiple"
-                            variant={'outline'}
-                            size={'lg'}
-                            value={data.categories}
-                            onValueChange={(value) => setData('categories', value)}
-                        >
-                            {categories.map((category) => (
-                                <ToggleGroupItem key={category.id} value={category.id.toString()}>
-                                    {category.name}
-                                </ToggleGroupItem>
-                            ))}
-                        </ToggleGroup>
+                            <div className="grid gap-2">
+                                <Label htmlFor="media">Media</Label>
 
-                        <InputError message={errors.due_date} />
-                    </div>
-                    <div className="flex items-center gap-4">
-                        <Button disabled={processing}>Create Task</Button>
-                    </div>
+                                <Input id="media" onChange={(e) => setData('media', e.target.files[0])} className="mt-1 block w-full" type="file" />
+
+                                {progress && (
+                                    <progress value={progress.percentage} max="100">
+                                        {progress.percentage}%
+                                    </progress>
+                                )}
+
+                                <InputError message={errors.media} />
+                            </div>
+
+                            <div className="grid gap-2">
+                                <Label htmlFor="due_date">Categories</Label>
+
+                                <ToggleGroup
+                                    type="multiple"
+                                    variant={'outline'}
+                                    size={'lg'}
+                                    value={data.categories}
+                                    onValueChange={(value) => setData('categories', value)}
+                                >
+                                    {categories.map((category) => (
+                                        <ToggleGroupItem key={category.id} value={category.id.toString()}>
+                                            {category.name}
+                                        </ToggleGroupItem>
+                                    ))}
+                                </ToggleGroup>
+
+                                <InputError message={errors.categories} />
+                            </div>
+                        </CardContent>
+
+                        <CardFooter>
+                            <Button disabled={processing}>Create Task</Button>
+                        </CardFooter>
+                    </Card>
                 </form>
             </div>
         </AppLayout>
